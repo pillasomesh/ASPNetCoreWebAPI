2f60a59de80633703e105fb68598bce3e75d774d
SomeswaraRao <pillasomesh@gmail.com>
2019-12-25 07:50:40 +0530

    Migration files

---
+++ AccountOwnerServer/Migrations/20191224140804_Migration_V2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AccountOwnerServer.Migrations
+{
+    public partial class Migration_V2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
